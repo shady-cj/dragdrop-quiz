4726e1827fc65d60ff33a2100ad2486cc3261aa2
shady-cj <petersp2000@gmail.com>
2021-09-13 18:26:39 +0100

    drag drop quiz

--- quiz.js
+++ quiz.js
@@ -1,7 +1,13 @@
+var numOfQuestions = 10
+var listOfWords=[]
+var currentQuestion = 0
+var mainPage = document.querySelector('.active')
+var pageBody = document.body
+var Loader = document.querySelector(".loader")
 var question=document.getElementById("question")
 var answer=document.getElementById("answer")
 var optBox = document.getElementById("optionsbox")
-var arrOfOptions=answer.textContent.trim().toUpperCase().split('')
+var arrOfOptions;
 var resultBox = document.querySelector('.resultbox')
 var arrOfOptionsEl = []
 var arrOfTipsIndex =[]
@@ -9,51 +15,119 @@ var arrOfTipsIndex =[]
 var wrong = false
 
 
-getArrayNumRange()
+
+// api key = b993a7fe-7718-4827-b933-c0283f6cc94b
+
+
+getWord()
 
 
 
 
 
-arrOfOptions.forEach(function(eacharr,index){
 
-    var newdiv=document.createElement('div')
-    var clonediv=newdiv.cloneNode(true)
+function getWord(){
+    fetch('http://127.0.0.1:9000/')
+    .then(response => response.json())
+    .then(result =>{
+        let dataLength = result.data.length
+        let arrayOfChosenIndex =[]
+        for (var i=0 ; i < numOfQuestions; i++){
+            var getRandIndex = uniqueRand(arrayOfChosenIndex,dataLength)
+            arrayOfChosenIndex.push(getRandIndex)
+            listOfWords.push(result.data[getRandIndex])
+        }
+        
+        getSyn()
+    })
+        
+    .catch(err=>{
+        console.log(err)
+   
+    })
 
-    generateResult(clonediv,eacharr,index)
-    newdiv.classList.add('option')
-    newdiv.setAttribute('data-id',index)
-    newdiv.setAttribute('draggable','true')
-    newdiv.innerHTML= eacharr
 
 
-    arrOfOptionsEl.push(newdiv)
+}
+
+function getSyn(){
+    searchWord = listOfWords[currentQuestion]
+    console.log(searchWord)
+    fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${searchWord}?key=b993a7fe-7718-4827-b933-c0283f6cc94b`)
+    .then(response=>response.json())
+    .then(data => {
+        targetSynonym=data[0]?.meta?.syns[0][2]
+        console.log(targetSynonym)
+        
+        if (targetSynonym){
+            question.innerHTML = `What is the synonym of the word <b style='color:red'> <i> ${searchWord} </i></b>`
+            arrOfOptions=targetSynonym.trim().split('')
+            getArrayNumRange()
+            getEachLetter()
+            pageBody.classList.remove('flex')
+            mainPage.classList.remove('hide')
+            Loader.classList.add('hide')
+            
+
+        }else{
+            currentQuestion++
+
+            return getSyn()
+        }
+    })
+    return;
+}
+
+
+
+
+function getEachLetter(){
+
+    arrOfOptions.forEach(function(eacharr,index){
+
+        var newdiv=document.createElement('div')
+        var clonediv=newdiv.cloneNode(true)
     
+        generateResult(clonediv,eacharr,index)
+        newdiv.classList.add('option')
+        newdiv.setAttribute('data-id',index)
+        newdiv.setAttribute('draggable','true')
+        newdiv.innerHTML= eacharr
+    
+    
+        arrOfOptionsEl.push(newdiv)
+        
+    
+        
+    })
 
     
-})
+    getEachEl()
+
+}
 
 
-var cloneArrayofoptionsEl=[...arrOfOptionsEl]
 
-cloneArrayofoptionsEl.map(a => ({value:a,sort:Math.random()})).sort((a,b)=>(a.sort-b.sort)).map(a => (a.value)).forEach(function(optionEl){
+
+function getEachEl(){
+
+    var cloneArrayofoptionsEl=[...arrOfOptionsEl]
+
+    cloneArrayofoptionsEl.map(a => ({value:a,sort:Math.random()})).sort((a,b)=>(a.sort-b.sort)).map(a => (a.value)).forEach(function(optionEl){
 
     optBox.appendChild(optionEl)
 
     optionEl.addEventListener('dragstart',dragStart)
 })
+}
 
 
 
 
-
-
-function uniqueRand(val){
-
+function uniqueRand(mainlist=[],val){
     var rand =  Math.floor(Math.random()*val)
-    if (arrOfTipsIndex.includes(rand)){
-
-        return uniqueRand(val)
+    if (mainlist.includes(rand)){
+        return uniqueRand(mainlist,val)
         
     }else{
 
@@ -72,9 +146,9 @@ function getArrayNumRange(){
     getNumOfTips=Math.floor(Math.random()*rangeOfNum)
 
     for (var i=0; i<getNumOfTips; i++){
-        var getRand = uniqueRand(lenOfArr)
-        
+        var getRand = uniqueRand(arrOfTipsIndex,lenOfArr)
         arrOfTipsIndex.push(getRand)
+        
 
     }
 }
@@ -103,12 +177,13 @@ function generateResult(divEl,arr,index){
 
 }
 
-var draggedObjContent;
-var droppedObj;
+var draggedObjContent, droppedObj,arrOfElId;
+
 function dragStart(){
 
     console.log('drag starts')
     draggedObjContent = this.textContent
+    arrOfElId= this.getAttribute("data-id")
 }
 
 function dragEnter(){
@@ -117,7 +192,6 @@ function dragEnter(){
 }
 function dragOver(e){
     e.preventDefault()
-    console.log('drag hover')
 }
 function dragDrop(){
 
@@ -142,6 +216,13 @@ function dragDrop(){
         this.textContent = innerLetter
         arrOfTipsIndex.push(+answerId)
         this.setAttribute('data-resultid',answerId)
+        arrOfOptionsEl[arrOfElId].style.visibility = "hidden"
+        this.classList.add('dropped')
+        this.addEventListener('animationend',function(){
+
+            this.classList.remove('dropped')
+        })
+
         if (arrOfTipsIndex.length === arrOfOptions.length){
             if (!wrong){
                 document.querySelectorAll('.result').forEach(function(result){
